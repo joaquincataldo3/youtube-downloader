69b1c22284b9559d0b5ab28371bc00e49fa4d1fd
joaquincataldo3 <jqncataldo@gmail.com>
2024-11-01 10:28:51 -0300

    adding puppeter and script to get cookies

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import ytdlp from 'yt-dlp-exec';
+import { getYoutubeCookies, writeNetscapeCookiesFile } from './utils.js';
+import fs from 'fs';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.post('/download', async (req, res, attempt = 1) => {
+    
     const { url } = req.body;
 
     if (!url) {
@@ -18,11 +21,15 @@ app.post('/download', async (req, res, attempt = 1) => {
 
     try {
 
+        const cookies = await getYoutubeCookies();
+
+        writeNetscapeCookiesFile(cookies)
+
         // sleeping interval to avoid getting status 429
         const videoProcess = ytdlp.exec(url, {
             output: '-', 
             sleepInterval: 5, 
-            cookies: './cookies.txt'
+            cookies: './youtube-cookies.txt'
         });
      
         videoProcess.stdout.pipe(res);
@@ -34,9 +41,17 @@ app.post('/download', async (req, res, attempt = 1) => {
 
         videoProcess.on('close', (code) => {
             console.log(`yt-dlp exited with code ${code}`);
+             fs.unlink('./youtube-cookies.txt', (err) => {
+                if (err) {
+                    console.error('Error deleting cookies.txt:', err);
+                } else {
+                    console.log('cookies.txt deleted successfully.');
+                }
+            });
              if (code !== 0 && !res.headersSent) {
                 return res.status(500).json({ message: 'Error downloading the video.' });
             }
+            
         });
     } catch (error) {
         console.error('Error executing yt-dlp:', error);
