48bcb40266708bd597098ce643b23a62a7ea4716
joaquincataldo3 <jqncataldo@gmail.com>
2024-11-08 12:42:45 -0300

    env

--- utils.js
+++ utils.js
@@ -15,9 +15,7 @@ puppeteer.use(Stealth());
 
 export const getYoutubeCookies = async () => {
     try{
-        const browser = await puppeteer.launch({
-            headless: false
-        })
+        const browser = await puppeteer.launch()
         const page = await browser.newPage();
         await page.setViewport({ width: 1920, height: 1080 });
         await page.setUserAgent(
