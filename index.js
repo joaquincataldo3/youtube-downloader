9c463ed9a47c84daf22c5a9e6e2bfad61f3d0ec0
joaquincataldo3 <jqncataldo@gmail.com>
2024-11-08 10:56:37 -0300

    changing just to obtain cookies

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import ytdlp from 'yt-dlp-exec';
-import { getYoutubeCookies, writeNetscapeCookiesFile } from './utils.js';
+import { formatCookiesToNetscape, getYoutubeCookies } from './utils.js';
 import fs from 'fs';
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,54 +15,16 @@ app.post('/download', async (req, res, attempt = 1) => {
         return res.status(400).json({ message: 'URL is required.' });
     }
 
-    res.setHeader('Content-Type', 'video/mp4');
-    res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
-
     try {
-
-        //const cookies = await getYoutubeCookies();
-
-        //writeNetscapeCookiesFile(cookies)
-
-        // sleeping interval to avoid getting status 429
-        const videoProcess = ytdlp.exec(url, {
-            output: '-', 
-            sleepInterval: 5, 
-            cookies: './youtube-cookies.txt'
-        });
-     
-        videoProcess.stdout.pipe(res);
-
-        videoProcess.stderr.on('data', (data) => {
-            console.log('stderr on')
-            console.log(data.toString()) 
-        });
-
-        videoProcess.on('close', (code) => {
-            console.log(`yt-dlp exited with code ${code}`);
-             fs.unlink('./youtube-cookies.txt', (err) => {
-                if (err) {
-                    console.error('Error deleting cookies.txt:', err);
-                } else {
-                    console.log('cookies.txt deleted successfully.');
-                }
-            });
-             if (code !== 0 && !res.headersSent) {
-                return res.status(500).json({ message: 'Error downloading the video.' });
-            }
-            
-        });
+       const cookies = await getYoutubeCookies();
+       const formatedCookies = formatCookiesToNetscape(cookies)
+       return res.status(200).json({
+            status: 'S',
+            data: formatedCookies
+       });
     } catch (error) {
-        console.error('Error executing yt-dlp:', error);
-        // limit the attemps to 3
-        if (error.message.includes('HTTP Error 429') && attempt < 3) { 
-            console.log(`Attempt ${attempt} failed. Retrying...`);
-            downloadVideo(url, res, attempt + 1);
-        } else {
-            if (!res.headersSent) {
-                return res.status(500).json({ message: 'Error executing yt-dlp.', error: error.message });
-            }
-        }
+        console.error('Error obtaining cookies:', error);
+        return 
     }
 });
 
